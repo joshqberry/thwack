71d0c2d648b645afff5d808f2033dd5436bc3ef4
Josh Qberry <joshqberry@gmail.com>
2020-09-07 16:25:20 -0600

    final (for now) adjustments to navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,7 @@ function toggle2() {
   document.getElementById("myDropdown2").classList.toggle("show");
 }
 
-function toggle3a() {
+function toggle3() {
   document.getElementById("myDropdown3").classList.toggle("show");
 }
 
